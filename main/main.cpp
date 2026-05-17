577eadd0beb47ef06ac7eb0a5f8aa7f1ebb647f8
Christoph.Oberle.ext <christoph.oberle@elrebo.de>
2026-05-17 18:25:00 +0200

    Version 1.0.4 - state of devices is traced in nvs_flash: power on/off executed only when needed

--- main/main.cpp
+++ main/main.cpp
@@ -15,6 +15,9 @@
 static const char *tag = "ESP AV RMT Controller";
 
 bool state = false;
+bool yamahaReceiver = false;
+bool panasonicTv = false;
+bool pioneerDvd = false;
 std::string activeScene = "OFF"; // {OFF, TV, AppleTV, Radio}
 
 // read value of key from NvsFlash
@@ -31,6 +34,22 @@ std::string readNvsFlashString(std::string tag, std::string space, std::string k
     return nvsRmt.GetStr(key, ret);
 }
 
+// read data from NvsFlash
+void readDataFromNvsFlash()
+{
+    esp_err_t ret;
+    state = (bool) readNvsFlashU8(std::string("nvsRmt"), std::string("rmt"), std::string("state"), &ret);
+    ESP_LOGI(tag, "state = %d", state);
+    yamahaReceiver = (bool) readNvsFlashU8(std::string("nvsRmt"), std::string("rmt"), std::string("yamahaReceiver"), &ret);
+    ESP_LOGI(tag, "yamahaReceiver = %d", yamahaReceiver);
+    panasonicTv = (bool) readNvsFlashU8(std::string("nvsRmt"), std::string("rmt"), std::string("panasonicTv"), &ret);
+    ESP_LOGI(tag, "panasonicTv = %d", panasonicTv);
+    pioneerDvd = (bool) readNvsFlashU8(std::string("nvsRmt"), std::string("rmt"), std::string("pioneerDvd"), &ret);
+    ESP_LOGI(tag, "pioneerDvd = %d", pioneerDvd);
+    std::string activeScene = readNvsFlashString(std::string("nvsRmt"), std::string("rmt"), std::string("activeScene"), &ret);
+    ESP_LOGI(tag, "activeScene = %s", activeScene.c_str());
+}
+
 // update value of key in NvsFlash
 esp_err_t updateNvsFlash(std::string tag, std::string space, std::string key, uint8_t value)
 {
@@ -44,15 +63,39 @@ esp_err_t updateNvsFlash(std::string tag, std::string space, std::string key, st
     return nvsRmt.SetStr(key, value);
 }
 
+void updateDataInNvsFlash() {
+    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("state"), state);
+    ESP_LOGI(tag, "state = %d", state);
+    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("yamahaReceiver"), yamahaReceiver);
+    ESP_LOGI(tag, "yamahaReceiver = %d", yamahaReceiver);
+    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("panasonicTv"), panasonicTv);
+    ESP_LOGI(tag, "panasonicTvr = %d", panasonicTv);
+    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("pioneerDvd"), pioneerDvd);
+    ESP_LOGI(tag, "pioneerDvd = %d", pioneerDvd);
+    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("activeScene"), activeScene);
+    ESP_LOGI(tag, "activeScene = %s", activeScene.c_str());
+}
+
 void switchAllOff(RmtIr* rmtIr) {
-        // YAMAHA Receiver
+    // YAMAHA Receiver
+    if (yamahaReceiver == true) {
         rmtIr->transmitNecCommandFrame((uint8_t)0x7a, (uint8_t)0x1e); // "STANDBY"
+        yamahaReceiver = false;
         vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
-        // Panasonic TV
+    }
+    // Panasonic TV
+    if (panasonicTv == true) {
         rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x00, 0xfc); // "Power Off"
+        panasonicTv = false;
         vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
+    }
         // Pioneer DVD Player
-        rmtIr->transmitPioneerCommandFrame((uint8_t)0xa3, (uint8_t)0x99, (uint8_t)0xaf, (uint8_t)0xbb); // "Shift"
+    if (pioneerDvd == true) {
+        rmtIr->transmitPioneerCommandFrame((uint8_t)0xa3, (uint8_t)0x99, (uint8_t)0xaf, (uint8_t)0xbb); // "Shift + OFF"
+        pioneerDvd = false;
+    }
+    activeScene = "OFF";
+    state = false;
 }
 
 // Callback function for BUTTON_SINGLE_CLICK event from onBoardButton
@@ -62,29 +105,29 @@ extern "C" void callback_onBoardButton_BUTTON_SINGLE_CLICK(void *arg, void *data
 
     iot_button_print_event((button_handle_t)arg);
 
-    // bei jedem BUTTON_SINGLE_CLICK wird der Fernseher ein-/ausgeschaltet
-    state = !state;
+    // bei jedem BUTTON_SINGLE_CLICK wird der Fernseher eingeschaltet
+    state = true;
     RmtIr* rmtIr = &rmtIr->getInstance(); // get the Singleton instance
-    if (!state) {
-        switchAllOff(rmtIr);
-        activeScene = "OFF";
+    if (pioneerDvd == true) {
+        rmtIr->transmitPioneerCommandFrame((uint8_t)0xa3, (uint8_t)0x99, (uint8_t)0xaf, (uint8_t)0xbb); // "Shift + OFF"
+        pioneerDvd = false;
+        vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
     }
-    else {
+    if (panasonicTv == false) {
         // Panasonic TV
         rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x00, 0x7c); // "Power On"
-        vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
-        // YAMAHA receiver
-        rmtIr->transmitNecCommandFrame((uint16_t)0x7a85, (uint16_t)0x037c); // "TV Scene"
-        // Panasonic TV
+        panasonicTv = true;
         vTaskDelay(pdMS_TO_TICKS(2000)); // delay 2 seconds
-        rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x40, 0x0c); // "TV"
-        activeScene = "TV";
     }
+    // YAMAHA receiver
+    rmtIr->transmitNecCommandFrame((uint16_t)0x7a85, (uint16_t)0x037c); // "TV Scene"
+    yamahaReceiver = true;
+    vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
+    // Panasonic TV
+    rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x40, 0x0c); // "TV"
+    activeScene = "TV";
 
-    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("state"), state);
-    ESP_LOGI(tag, "state = %d", state);
-    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("activeScene"), activeScene);
-    ESP_LOGI(tag, "activeScene = %s", activeScene.c_str());
+    updateDataInNvsFlash();
 }
 
 // Callback function for BUTTON_DOUBLE_CLICK event from onBoardButton
@@ -94,30 +137,31 @@ extern "C" void callback_onBoardButton_BUTTON_DOUBLE_CLICK(void *arg, void *data
 
     iot_button_print_event((button_handle_t)arg);
 
-    // bei jedem BUTTON_DOUBLE_CLICK wird der AppleTV ein-/ausgeschaltet
-    state = !state;
+    // bei jedem BUTTON_DOUBLE_CLICK wird der Eingang für AppleTV eingeschaltet
+    state = true;
     ESP_LOGI(tag, "state = %d", state);
     RmtIr* rmtIr = &rmtIr->getInstance(); // get the Singleton instance
-    if (!state) {
-        switchAllOff(rmtIr);
-        activeScene = "OFF";
+
+    if (pioneerDvd == true) {
+        rmtIr->transmitPioneerCommandFrame((uint8_t)0xa3, (uint8_t)0x99, (uint8_t)0xaf, (uint8_t)0xbb); // "Shift + OFF"
+        pioneerDvd = false;
+        vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
     }
-    else {
+    if (panasonicTv == false) {
         // Panasonic TV
         rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x00, 0x7c); // "Power On"
-        // YAMAHA receiver
-        vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
-        rmtIr->transmitNecCommandFrame((uint16_t)0x7a85, (uint16_t)0x0976); // "TV Scene"
-        // Panasonic TV
+        panasonicTv = true;
         vTaskDelay(pdMS_TO_TICKS(2000)); // delay 2 seconds
-        rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x20, 0x0d); // "HDMI1" (direct)
-        activeScene = "AppleTV";
     }
+    // YAMAHA receiver
+    rmtIr->transmitNecCommandFrame((uint16_t)0x7a85, (uint16_t)0x0976); // "Radio Scene" (AppleTV)
+    yamahaReceiver = true;
+    vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
+    // Panasonic TV
+    rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x20, 0x0d); // "HDMI1" (direct)
+    activeScene = "AppleTV";
 
-    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("state"), state);
-    ESP_LOGI(tag, "state = %d", state);
-    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("activeScene"), activeScene);
-    ESP_LOGI(tag, "activeScene = %s", activeScene.c_str());
+    updateDataInNvsFlash();
 }
 
 // Callback function for BUTTON_MULTIPLE_CLICK_3 event from onBoardButton
@@ -127,65 +171,89 @@ extern "C" void callback_onBoardButton_BUTTON_MULTIPLE_CLICK_3(void *arg, void *
 
     iot_button_print_event((button_handle_t)arg);
 
-    // bei jedem BUTTON_MULTIPLE_CLICK_3 wird der DVD-Spieler ein-/ausgeschaltet
-    state = !state;
+    // bei jedem BUTTON_MULTIPLE_CLICK_3 wird der DVD-Spieler eingeschaltet
+    state = true;
     ESP_LOGI(tag, "state = %d", state);
     RmtIr* rmtIr = &rmtIr->getInstance(); // get the Singleton instance
-    if (!state) {
-        switchAllOff(rmtIr);
-        activeScene = "OFF";
-   }
-    else {
+
+    if (panasonicTv == false) {
         // Panasonic TV
         rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x00, 0x7c); // "Power On"
-        // Pioneer DVD Player
-        vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
-        rmtIr->transmitPioneerCommandFrame((uint8_t)0xa3, (uint8_t)0x99, (uint8_t)0xaf, (uint8_t)0xba); // "ON"
-        // YAMAHA Receiver
-        vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
-        rmtIr->transmitNecCommandFrame((uint16_t)0x7a85, (uint16_t)0x007f); // "BD/DVD Scene"
-        // Pioneer DVD Player
-        vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
-        rmtIr->transmitPioneerCommandFrame((uint8_t)0xa3, (uint8_t)0x99, (uint8_t)0xaf, (uint8_t)0xb6); // "OPEN/CLOSE"
-        // Panasonic TV
-        vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
-        rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x00, 0x40); // "AV2" (direct)
-        activeScene = "DVD";
+        panasonicTv = true;
+        vTaskDelay(pdMS_TO_TICKS(1000)); // delay 1 seconds
     }
+    if(pioneerDvd == false) {
+        // Pioneer DVD Player
+        rmtIr->transmitPioneerCommandFrame((uint8_t)0xa3, (uint8_t)0x99, (uint8_t)0xaf, (uint8_t)0xba); // "ON"
+        pioneerDvd = true;
+        vTaskDelay(pdMS_TO_TICKS(1000)); // delay 1 seconds
+    }
+    // YAMAHA Receiver
+    rmtIr->transmitNecCommandFrame((uint16_t)0x7a85, (uint16_t)0x007f); // "BD/DVD Scene"
+    yamahaReceiver = true;
+    vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
+    // Pioneer DVD Player
+    rmtIr->transmitPioneerCommandFrame((uint8_t)0xa3, (uint8_t)0x99, (uint8_t)0xaf, (uint8_t)0xb6); // "OPEN/CLOSE"
+    vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
+    // Panasonic TV
+    rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x00, 0x40); // "AV2" (direct)
+    activeScene = "DVD";
 
-    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("state"), state);
-    ESP_LOGI(tag, "state = %d", state);
-    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("activeScene"), activeScene);
-    ESP_LOGI(tag, "activeScene = %s", activeScene.c_str());
+    updateDataInNvsFlash();
 }
 
-// Callback function for BUTTON_LONG_PRESS_START event from onBoardButton
-extern "C" void callback_onBoardButton_BUTTON_LONG_PRESS_START_1000(void *arg, void *data)
+// Callback function for BUTTON_LONG_PRESS_UP_1000 event from onBoardButton
+extern "C" void callback_onBoardButton_BUTTON_LONG_PRESS_UP_1000(void *arg, void *data)
 {
-    ESP_LOGI("onBoardButton Callback", "for Event BUTTON_LONG_PRESS_START_1000 called!");
+    ESP_LOGI("onBoardButton Callback", "for Event BUTTON_LONG_PRESS_UP_1000 called!");
 
     iot_button_print_event((button_handle_t)arg);
 
-    // bei jedem BUTTON_LONG_PRESS_START wird der state umgeschaltet
-    state = !state;
+    // bei jedem BUTTON_LONG_PRESS_UP_1000 wird der Tuner eingeschaltet
+    state = true;
     ESP_LOGI(tag, "state = %d", state);
     RmtIr* rmtIr = &rmtIr->getInstance(); // get the Singleton instance
 
-    if (!state) {
-        switchAllOff(rmtIr);
-        activeScene = "OFF";
-    }
-    else {
+    if (yamahaReceiver == false) {
         rmtIr->transmitNecCommandFrame((uint8_t)0x7a, (uint8_t)0x1d); // "POWER ON"
+        yamahaReceiver = true;
         vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
-        rmtIr->transmitNecCommandFrame((uint8_t)0x7a, (uint8_t)0x16); // "Tuner"
-        activeScene = "Radio";
     }
 
-    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("state"), state);
+    if (pioneerDvd == true) {
+        rmtIr->transmitPioneerCommandFrame((uint8_t)0xa3, (uint8_t)0x99, (uint8_t)0xaf, (uint8_t)0xbb); // "Shift + OFF"
+        pioneerDvd = false;
+        vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
+    }
+
+    // Panasonic TV
+    if (panasonicTv == true) {
+        rmtIr->transmitPanasonicCommandFrame(0x4004, 0x01, 0x00, 0xfc); // "Power Off"
+        panasonicTv = false;
+        vTaskDelay(pdMS_TO_TICKS(500)); // delay 0.5 seconds
+    }
+
+    rmtIr->transmitNecCommandFrame((uint8_t)0x7a, (uint8_t)0x16); // "Tuner"
+    activeScene = "Radio";
+
+    updateDataInNvsFlash();
+}
+
+// Callback function for BUTTON_LONG_PRESS_UP event from onBoardButton
+extern "C" void callback_onBoardButton_BUTTON_LONG_PRESS_START_3000(void *arg, void *data)
+{
+    ESP_LOGI("onBoardButton Callback", "for Event BUTTON_LONG_PRESS_START_3000 called!");
+
+    iot_button_print_event((button_handle_t)arg);
+
+    // bei jedem BUTTON_LONG_PRESS_START_3000 wird alles ausgeschaltet
+    state = false;
     ESP_LOGI(tag, "state = %d", state);
-    updateNvsFlash(std::string("nvsRmt"), std::string("rmt"), std::string("activeScene"), activeScene);
-    ESP_LOGI(tag, "activeScene = %s", activeScene.c_str());
+    RmtIr* rmtIr = &rmtIr->getInstance(); // get the Singleton instance
+
+    switchAllOff(rmtIr);
+
+    updateDataInNvsFlash();
 }
 
 extern "C" void app_main(void)
@@ -214,12 +282,18 @@ extern "C" void app_main(void)
 
     onBoardButton.RegisterCallbackForEvent(BUTTON_SINGLE_CLICK, callback_onBoardButton_BUTTON_SINGLE_CLICK);
     onBoardButton.RegisterCallbackForEvent(BUTTON_DOUBLE_CLICK, callback_onBoardButton_BUTTON_DOUBLE_CLICK);
-    button_event_args_t press_time = {
+    button_event_args_t press_time_1000 = {
        { // long_press
            1000, // press_time
        }
     };
-    onBoardButton.RegisterCallbackForEvent(BUTTON_LONG_PRESS_START, &press_time, callback_onBoardButton_BUTTON_LONG_PRESS_START_1000);
+    onBoardButton.RegisterCallbackForEvent(BUTTON_LONG_PRESS_UP, &press_time_1000, callback_onBoardButton_BUTTON_LONG_PRESS_UP_1000);
+    button_event_args_t press_time_3000 = {
+       { // long_press
+           3000, // press_time
+       }
+    };
+    onBoardButton.RegisterCallbackForEvent(BUTTON_LONG_PRESS_START, &press_time_3000, callback_onBoardButton_BUTTON_LONG_PRESS_START_3000);
     button_event_args_t clicks = {
        { // multiple_clicks
            3, // clicks
@@ -227,21 +301,8 @@ extern "C" void app_main(void)
     };
     onBoardButton.RegisterCallbackForEvent(BUTTON_MULTIPLE_CLICK, &clicks, callback_onBoardButton_BUTTON_MULTIPLE_CLICK_3);
 
-    // read state and activeScene from nvs_flash
-    {
-        esp_err_t ret = ESP_OK;
-        uint8_t nvsState = readNvsFlashU8(std::string("nvsRmt"), std::string("rmt"), std::string("state"), &ret);
-        if (ret == ESP_OK) {
-            state = (bool) nvsState;
-        }
-        ret = ESP_OK;
-        std::string nvsActiveScene = readNvsFlashString(std::string("nvsRmt"), std::string("rmt"), std::string("activeScene"), &ret);
-        if (ret == ESP_OK) {
-            activeScene = nvsActiveScene;
-        }
-        ESP_LOGI(tag, "state = %d", state);
-        ESP_LOGI(tag, "activeScene = %s", activeScene.c_str());
-    }
+    // read data from nvs_flash
+    readDataFromNvsFlash();
 
     // receiver test
     while(1) {
